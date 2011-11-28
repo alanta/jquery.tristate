9468710d04fc10f90ce4123b935745744d804ec3
Marnix van Valen <marnix@alanta.nl>
2011-11-28 15:30:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+_Tri-state checkbox input using jQuery_
+
+This is a jQuery plugin that allows you to use tristate checkboxes. 
+It was designed for use in master-detail scenario's to enable quick check all / uncheck all functionality.
+
+__Usage__
+Please view the source code in sample.html for samples of how to use this control.
---
+++ js/jquery.tristate.js
@@ -0,0 +1,81 @@
+/*
+ *	Tri-state checkbox input using jQuery
+ *	Version 1.0
+ *
+ *	Author  : Marnix van Valen, Alanta, Tilburg
+ *	E-mail  : marnix@alanta.nl
+ *	Source  : github.com/alanta/jquery.tristate
+ *
+ *	License : MIT - http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+(function ($){
+	var methods = {
+		init : function( options ) {
+			return this.each(function(){
+				//var config = {};
+				//var opts = $.extend(config,options);
+				var obj = $(this);
+				obj.click(methods.onClick);
+				var initialState = methods.getState.call(this);
+                methods._apply.call(this, initialState);
+			});
+		},
+		onClick: function(e) {
+			e.preventDefault();
+			var state = methods.getState.call(this);
+			switch( state ){
+				case 'on':
+					methods._apply.call(this,'off');
+					break;
+				case 'intermediate':
+				case 'off':
+				default:
+					methods._apply.call(this,'on');
+					break;
+			}
+			$('input[type=hidden]', this).trigger('change');
+		},
+		getState : function(){
+			var obj = $(this);
+			return obj.hasClass('on') ? 'on' : 
+			( obj.hasClass('intermediate') ? 'intermediate' : 'off' );
+		},
+		setState : function(value){
+			methods._apply.call(this,value);
+			return $(this);
+		},
+		_apply : function(newState){
+			var obj = $(this);
+			var input = $('input[type=hidden]', obj);
+			switch(newState){
+				case 'on':
+				case true:
+					obj.removeClass('off intermediate').addClass('on');
+					input.val('true');
+					break;
+				case 'intermediate':
+					obj.removeClass('off on').addClass('intermediate');
+					input.val('true');
+					break;
+				default:
+					obj.removeClass('on intermediate').addClass('off');
+					input.val('false');
+					break;
+			}
+		}
+	};
+
+	$.fn.tristate = function(method) {  
+		// Method calling logic
+		if ( methods[method] ) {
+		  return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( method === 'intermediate' || method === 'on' || method === 'off' || method === true || method === false ) {
+			return methods.setState.call(this,method);
+		} else if ( typeof method === 'object' || ! method ) {
+		  return methods.init.apply( this, arguments );
+		} else {
+		  $.error( 'Method ' +  method + ' does not exist on jQuery.tristate' );
+		} 
+	};
+})(jQuery)
